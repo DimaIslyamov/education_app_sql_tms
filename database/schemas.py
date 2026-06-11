7d3a05e56a74471ed988afa4d4a28a29a7a737d7
Evgeni <thehacjlednik@gmail.com>
2026-06-11 20:54:30 +0300

    fix

--- database/schemas.py
+++ database/schemas.py
@@ -6,6 +6,14 @@ CREATE TABLE IF NOT EXISTS directions (
     name        TEXT    NOT NULL UNIQUE,
     description TEXT    NOT NULL DEFAULT ''
 );
+
+CREATE TABLE IF NOT EXISTS teachers ( 
+    id	INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name	TEXT 	NOT NULL,
+    last_name	TEXT	NOT NULL,
+    email	    TEXT	NOT NULL,
+    phone   	TEXT	NOT NULL
+);
 """
 
 def init_schema(db: Database) -> None:
@@ -16,3 +24,4 @@ def init_schema(db: Database) -> None:
 if __name__ == "__main__":
     with Database() as db:
         init_schema(db)
+
